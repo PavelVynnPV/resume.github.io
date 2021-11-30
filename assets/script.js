97b63438c807243e8e6666c76e0741f9a68d2b88
Pavel <92670880+PavelVynnPV@users.noreply.github.com>
2021-11-30 14:27:12 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, );
+  });
+
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
   });
